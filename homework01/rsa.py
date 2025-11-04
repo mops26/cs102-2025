e5b325eec69c22d64f3017f06c7de4e994c6597e
mops26 <ol.morozoff2@gmail.com>
2025-11-04 22:26:40 +0300

    Реализована функция is_prime(n)

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -1,3 +1,4 @@
+import math
 import random
 import typing as tp
 
@@ -12,9 +13,18 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
-    pass
-
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    i = 3
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+    return True
 
 def gcd(a: int, b: int) -> int:
     """
--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,7 +9,29 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    key_length = len(keyword)
+    key_index = 0
+
+    for char in plaintext:
+        if char.isalpha():
+            key_char = keyword[key_index]
+            shift = ord(key_char) - ord('A')
+            if char.isupper():
+                new_char_code = ord(char) + shift
+                if new_char_code > ord('Z'):
+                    new_char_code -= 26
+                ciphertext += chr(new_char_code)
+            else:
+                new_char_code = ord(char) + shift
+                if new_char_code > ord('z'):
+                    new_char_code -= 26
+                ciphertext += chr(new_char_code)
+            key_index += 1
+            if key_index == key_length:
+                key_index = 0
+        else:
+            ciphertext += char
     return ciphertext
 
 
@@ -24,5 +46,29 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+
+    keyword = keyword.upper()
+    key_length = len(keyword)
+    key_index = 0
+
+    for char in ciphertext:
+        if char.isalpha():
+            key_char = keyword[key_index]
+            shift = ord(key_char) - ord('A')
+            if char.isupper():
+                new_char_code = ord(char) - shift
+                if new_char_code < ord('A'):
+                    new_char_code += 26
+                plaintext += chr(new_char_code)
+            else:
+                new_char_code = ord(char) - shift
+                if new_char_code < ord('a'):
+                    new_char_code += 26
+                plaintext += chr(new_char_code)
+            key_index += 1
+            if key_index == key_length:
+                key_index = 0
+        else:
+            plaintext += char
+
     return plaintext
