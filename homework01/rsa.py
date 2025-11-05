4454fb666b52863e234ce0971326711bcbffb030
mops26 <ol.morozoff2@gmail.com>
2025-11-05 14:04:49 +0300

    Исправлены ошибки

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -60,7 +60,6 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     return x % phi
 
 
-
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
     if not (is_prime(p) and is_prime(q)):
         raise ValueError("Both numbers must be prime.")
@@ -104,7 +103,7 @@ def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
     # Unpack the key into its components
     key, n = pk
     # Generate the plaintext based on the ciphertext and key using a^b mod m
-    plain = [chr((char ** key) % n) for char in ciphertext]
+    plain = [chr((char**key) % n) for char in ciphertext]
     # Return the array of bytes as a string
     return "".join(plain)
 
