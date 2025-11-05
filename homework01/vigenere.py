dc0f3be213b8dc8924cf60654a543f977ae955ca
mops26 <ol.morozoff2@gmail.com>
2025-11-05 14:03:08 +0300

    Исправлены ошибки

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -11,20 +11,20 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     ciphertext = ""
     key_length = len(keyword)
     keyword = keyword.upper()
-    multiplier = len(plaintext) //  key_length + 1
+    multiplier = len(plaintext) // key_length + 1
     new_key = keyword * multiplier
 
     for char, key_char in zip(plaintext, new_key):
         if char.isalpha():
-            shift = ord(key_char) - ord('A')
+            shift = ord(key_char) - ord("A")
             if char.isupper():
                 new_char_code = ord(char) + shift
-                if new_char_code > ord('Z'):
+                if new_char_code > ord("Z"):
                     new_char_code -= 26
                 ciphertext += chr(new_char_code)
             else:
                 new_char_code = ord(char) + shift
-                if new_char_code > ord('z'):
+                if new_char_code > ord("z"):
                     new_char_code -= 26
                 ciphertext += chr(new_char_code)
         else:
@@ -51,15 +51,15 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
 
     for char, key_char in zip(ciphertext, new_key):
         if char.isalpha():
-            shift = ord(key_char) - ord('A')
+            shift = ord(key_char) - ord("A")
             if char.isupper():
                 new_char_code = ord(char) - shift
-                if new_char_code < ord('A'):
+                if new_char_code < ord("A"):
                     new_char_code += 26
                 plaintext += chr(new_char_code)
             else:
                 new_char_code = ord(char) - shift
-                if new_char_code < ord('a'):
+                if new_char_code < ord("a"):
                     new_char_code += 26
                 plaintext += chr(new_char_code)
         else:
