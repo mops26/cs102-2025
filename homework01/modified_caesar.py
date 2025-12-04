a26021dc69fa8ead4545f0d352a2745d7cc7a7c6
mops26 <ol.morozoff2@gmail.com>
2025-12-04 14:49:17 +0300

    Исправлена проблема с границами у больших текстов

--- homework01/modified_caesar.py
+++ homework01/modified_caesar.py
@@ -1,17 +1,24 @@
 def decrypt_growing_shift(ciphertext: str, start: int, delta: int) -> str:
     plaintext = ""
     current_shift = start
+    first_up, last_up = ord("А"), ord("Я")
+    first_low, last_low = ord("а"), ord("я")
+    alphabet_size = 32
     for char in ciphertext:
         if char.isalpha():
             if char.isupper():
-                first, last = ord("А"), ord("Я")
+                first, last = first_up, last_up
             else:
-                first, last = ord("а"), ord("я")
+                first, last = first_low, last_low
             new_code = ord(char) - current_shift
-            if new_code > last:
-                new_code -= 32
             if new_code < first:
-                new_code += 32
+                diff = first - new_code
+                cycles = (diff - 1) // alphabet_size + 1  # смотрим, на сколько полных алфавитов мы отдалились
+                new_code += cycles * alphabet_size
+            if new_code > last:
+                diff = new_code - last
+                cycles = (diff - 1) // alphabet_size + 1
+                new_code -= cycles * alphabet_size
             current_shift += delta
             plaintext += chr(new_code)
         else:
