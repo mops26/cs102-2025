d45459fcb025ddea0623d72d0f696be2ee0d7a5b
mops26 <ol.morozoff2@gmail.com>
2026-01-14 15:05:18 +0300

    Реализован прототип игры

--- homework04/life_proto.py
+++ homework04/life_proto.py
@@ -10,9 +10,7 @@ Grid = tp.List[Cells]
 
 
 class GameOfLife:
-    def __init__(
-        self, width: int = 640, height: int = 480, cell_size: int = 10, speed: int = 10
-    ) -> None:
+    def __init__(self, width: int = 640, height: int = 480, cell_size: int = 10, speed: int = 10) -> None:
         self.width = width
         self.height = height
         self.cell_size = cell_size
@@ -28,35 +26,31 @@ class GameOfLife:
 
         # Скорость протекания игры
         self.speed = speed
+        self.grid = self.create_grid(randomize=True)
 
     def draw_lines(self) -> None:
-        """ Отрисовать сетку """
+        """Отрисовать сетку"""
         for x in range(0, self.width, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
         for y in range(0, self.height, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
 
     def run(self) -> None:
-        """ Запустить игру """
+        """Запустить игру"""
         pygame.init()
         clock = pygame.time.Clock()
         pygame.display.set_caption("Game of Life")
         self.screen.fill(pygame.Color("white"))
-
-        # Создание списка клеток
-        # PUT YOUR CODE HERE
-
+        self.create_grid()
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == QUIT:
                     running = False
+            self.screen.fill(pygame.Color("white"))
+            self.draw_grid()
             self.draw_lines()
-
-            # Отрисовка списка клеток
-            # Выполнение одного шага игры (обновление состояния ячеек)
-            # PUT YOUR CODE HERE
-
+            self.grid = self.get_next_generation()
             pygame.display.flip()
             clock.tick(self.speed)
         pygame.quit()
@@ -79,13 +73,22 @@ class GameOfLife:
         out : Grid
             Матрица клеток размером `cell_height` х `cell_width`.
         """
-        pass
+        if randomize:
+            grid = [[random.randint(0, 1) for _ in range(self.cell_width)] for _ in range(self.cell_height)]
+        else:
+            grid = [[0 for _ in range(self.cell_width)] for _ in range(self.cell_height)]
+        return grid
 
     def draw_grid(self) -> None:
         """
         Отрисовка списка клеток с закрашиванием их в соответствующе цвета.
         """
-        pass
+        for x in range(self.cell_height):
+            for y in range(self.cell_width):
+                coord_x = x * self.cell_size
+                coord_y = y * self.cell_size
+                color = pygame.Color("green") if self.grid[x][y] == 1 else pygame.Color("white")
+                pygame.draw.rect(self.screen, color, (coord_x, coord_y, self.cell_size, self.cell_size))
 
     def get_neighbours(self, cell: Cell) -> Cells:
         """
@@ -103,9 +106,15 @@ class GameOfLife:
         Returns
         ----------
         out : Cells
-            Список соседних клеток.
+            Список соседних клеток, в котором каждая позиция – 0 или 1.
         """
-        pass
+        neighbours = []
+        x, y = cell
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
+        for i, j in directions:
+            if 0 <= x + i < self.cell_height and 0 <= y + j < self.cell_width:
+                neighbours.append(self.grid[x + i][y + j])
+        return neighbours
 
     def get_next_generation(self) -> Grid:
         """
@@ -116,4 +125,12 @@ class GameOfLife:
         out : Grid
             Новое поколение клеток.
         """
-        pass
+        next_generation = [[0 for _ in range(self.cell_width)] for _ in range(self.cell_height)]
+        for i in range(self.cell_height):
+            for j in range(self.cell_width):
+                cells = sum(self.get_neighbours((i, j)))
+                if (self.grid[i][j] == 1 and cells in (2, 3)) or (self.grid[i][j] == 0 and cells == 3):
+                    next_generation[i][j] = 1
+                else:
+                    next_generation[i][j] = 0
+        return next_generation
